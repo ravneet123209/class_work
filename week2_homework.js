322f74178d59b0c282ea4c8fb30536bc7c8bdf70
Ravneet Kaur <harpreetrandhawa70777@gmail.com>
2026-03-05 13:10:43 +0100

    solution when login success

---
+++ week2_homework.js
@@ -0,0 +1,13 @@
+//Task 1 - Advanced Login System
+let username = "admin"; 
+let password = 1234;
+let isActive = true;
+
+if(username ==="admin" && password ===1234 && isActive ===true){
+    console.log("Login Successful");
+}else if(username ==="admin" && password ===1234 && isActive ===false){
+    console.log("Account inactive");
+}else{
+    console.log("Invalid credentials");
+}
+
