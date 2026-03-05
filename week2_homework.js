8ab08d4a5b40835c021a13dda2e926bc46ebd416
ravneet123209 <harpreetrandhawa70777@gmail.com>
2026-03-05 13:39:40 +0100

    solution when password is wrong

--- week2_homework.js
+++ week2_homework.js
@@ -1,6 +1,6 @@
 //Task 1 - Advanced Login System
 let username = "admin"; 
-let password = 1234;
+let password = 12345;
 let isActive = true;
 
 if(username ==="admin" && password ===1234 && isActive ===true){
